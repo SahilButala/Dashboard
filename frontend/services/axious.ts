7ae52039724e6ab5a30c6ef2cf3616d8967fdd80
sahil <sahil06butala@gmail.com>
2025-06-14 13:12:47 +0530

    frontend ul change

--- frontend/services/axious.ts
+++ frontend/services/axious.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const url = "http://localhost:3000";
+// const url = "http://localhost:3000";
+const url = "https://dashboard-backend-6fh1.onrender.com"
 // const url = 'http://localhost:3000';
 
 const axiosInstance = axios.create({
