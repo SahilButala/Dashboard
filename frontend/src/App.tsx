ca2387e54a9fede8a500a3c898f4d839857b835b
sahil <sahil06butala@gmail.com>
2025-06-14 20:03:36 +0530

    small changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,6 @@ import { SignIn } from "./pages/SignIn";
 import SignUp from "./components/SignUp";
 import CreateProduct from "./pages/CreateProduct";
 import ProtectedRoute from "./components/util/ProtectedRoute";
-import Navbar from "./components/Navbar";
 import { useSelector } from "react-redux";
 import type { RootState } from "./store";
 
